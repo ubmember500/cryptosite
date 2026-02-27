23d5657340420cd1eb41c6081886e83bfac846a8
ubmember500 <defensivelox@gmail.com>
2026-02-27 17:23:42 +0300

    feat: add Support button with Telegram link to account menu

--- client/src/components/common/UserAccountMenu.jsx
+++ client/src/components/common/UserAccountMenu.jsx
@@ -87,6 +87,18 @@ const UserAccountMenu = ({ chipClassName = '', menuClassName = '' }) => {
             <Bot className="h-4 w-4 text-textSecondary shrink-0" />
             {t('Telegram Bots')}
           </Link>
+          <a
+            href="https://t.me/ManagerAlan"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={closeMenu}
+            className="flex items-center gap-3 px-4 py-2.5 text-sm text-textPrimary hover:bg-surfaceHover transition-colors"
+          >
+            <svg className="h-4 w-4 shrink-0" viewBox="0 0 24 24" fill="#229ED9" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.447 1.394c-.16.16-.295.295-.605.295l.213-3.053 5.56-5.023c.242-.213-.054-.333-.373-.12L7.17 13.93l-2.956-.924c-.64-.203-.654-.64.136-.95l11.57-4.461c.537-.194 1.006.131.974.626z"/>
+            </svg>
+            {t('Support')}
+          </a>
           <button
             type="button"
             onClick={handleLogout}
